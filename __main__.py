6f54df34ffb0624b7f907edcb79d4c9debf37080
Ivan Vozhakov <gou177@bk.ru>
2024-02-02 03:42:17 +0500

    feat: Use rewire_fastapi

--- __main__.py
+++ __main__.py
@@ -3,10 +3,11 @@ from rewire.dependencies import DependenciesModule
 from rewire.lifecycle import LifecycleModule
 from rewire.loader import LoaderModule
 from rewire.space import Space
-
+import rewire_fastapi
 
 async def main():
     async with Space().init().use():
+        DependenciesModule.get().add(rewire_fastapi.plugin)
         await LoaderModule.get().discover().load()
         await DependenciesModule.get().solve()
         await LifecycleModule.get().start()
